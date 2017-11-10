4a39fdcaf2dbbf2c329c9897524b8d5202267ab6
Mike <zhaoming23@github.com>
2017-11-10 16:59:08 +0800

    示例

--- src/main/java/io/mike/Launch.java
+++ src/main/java/io/mike/Launch.java
@@ -1,6 +1,7 @@
 package io.mike;
 
 import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,10 +15,18 @@ public class Launch {
 	private static Logger log = Logger.getLogger("io.mike");
 	
 	public static void main(String[] args) throws SecurityException, IOException {
-		//-Djava.util.logging.config.file=src/main/resources/logging.properties
+		/**
+		 *	JVM 需要添加如下配置：
+		 *	-Djava.util.logging.config.file=src/main/resources/logging.properties
+		 */
 		System.out.println(System.getProperty("java.util.logging.config.file"));
         log.log(Level.INFO, "hello!1");
 		log.log(Level.INFO, "hello!2");
+		
+		//可以代码方式添加一个日志文件
+		FileHandler fileHandler = new FileHandler("./file-mike-%u.log");
+		log.addHandler(fileHandler);
+		log.log(Level.INFO, "hello!3");
 	}
 	
 
